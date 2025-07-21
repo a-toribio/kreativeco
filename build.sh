1d8de7e9055df5d8f3ffe34929ac85f6ed1b612c
kreativeco <kreativeco.store@gmail.com>
2025-07-21 19:15:30 +0200

    21.07

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # build.sh
-python manage.py collectstatic --no-input
+# Instala dependencias
+pip install -r requirements.txt
+
+# Aplica migraciones
+python manage.py migrate
+
+# Recolecta archivos estáticos
+python manage.py collectstatic --noinput
