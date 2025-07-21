4397a8c73282d557862e509f27ebbd6f90633213
kreativeco <kreativeco.store@gmail.com>
2025-07-21 18:54:39 +0200

    1.13

--- kreativeco/settings.py
+++ kreativeco/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m!zhy=qo@0y3ofc@8=2m0n%ocs9dkmzg(v#de-xc^2e3z4z$y-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kreativeco.onrender.com']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tienda', 'static')]
 
 # Archivos multimedia (como fotos de productos)
